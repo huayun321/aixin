26a2c5496815806a46be534f79d6cb3140f9a970
huayun321 <huayun321@gmail.com>
2017-05-12 14:51:41 +0800

    add jwt middleware

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"os"
 
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	jwt "github.com/dgrijalva/jwt-go"
 	nigronimgosession "github.com/joeljames/nigroni-mgo-session"
 	"github.com/unrolled/render"
 
@@ -66,6 +68,8 @@ func bindingHandler(resp http.ResponseWriter, req *http.Request) {
 }
 
 //===================== binding end
+
+//===================== nms start
 func signInHandler(resp http.ResponseWriter, req *http.Request) {
 	lf := new(LoginForm)
 	if errs := binding.Bind(req, lf); errs != nil {
@@ -91,6 +95,56 @@ func usersHandler(resp http.ResponseWriter, req *http.Request) {
 	}{list})
 }
 
+//===================== nms end
+
+//===================== jwt start
+func jwtOnError(w http.ResponseWriter, r *http.Request, err string) {
+	ren.JSON(w, http.StatusUnauthorized, map[string]interface{}{"code": 001, "msg": "未通过认证的请求", "err": "unauthorized"})
+}
+
+func jwtLoginHandler(resp http.ResponseWriter, req *http.Request) {
+	lf := new(LoginForm)
+	if errs := binding.Bind(req, lf); errs != nil {
+		http.Error(resp, errs.Error(), http.StatusBadRequest)
+		return
+	}
+
+	//check username password
+	if lf.Username != "admin" && lf.Password != "admin123" {
+		ren.JSON(resp, http.StatusBadRequest, map[string]interface{}{"code": 001, "msg": "用户名或密码错误", "err": "username password not match"})
+		return
+	}
+
+	// Claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": "asdf",
+		"admin":   "true",
+	})
+
+	// Headers
+	token.Header["alg"] = "HS256"
+	token.Header["typ"] = "JWT"
+
+	//sign
+	tokenString, err := token.SignedString([]byte("My Secret"))
+	if err != nil {
+		fmt.Fprintf(resp, "token err: %v", err)
+		return
+	}
+
+	err = ren.JSON(resp, http.StatusOK, map[string]interface{}{"code": 0, "msg": "ok", "token": tokenString})
+	if err != nil {
+		fmt.Fprint(resp, "something wrong")
+	}
+}
+
+func jwtSecuredHandler(w http.ResponseWriter, r *http.Request) {
+	user := r.Context().Value("user")
+	fmt.Println(user)
+	ren.JSON(w, http.StatusOK, "All good. You only get this message if you're authenticated")
+}
+
+//===================== jwt end
 //test
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("hello")
@@ -99,7 +153,20 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	n := negroni.Classic()
+	//jwt middleware
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return []byte("My Secret"), nil
+		},
+		// When set, the middleware verifies that tokens are signed with the specific signing algorithm
+		// If the signing method is not constant the ValidationKeyGetter callback can be used to implement additional checks
+		// Important to avoid security issues described here: https://auth0.com/blog/2015/03/31/critical-vulnerabilities-in-json-web-token-libraries/
+		SigningMethod: jwt.SigningMethodHS256,
 
+		ErrorHandler: jwtOnError,
+	})
+
+	//nms middleware
 	dbAccessor, err := nigronimgosession.NewDatabaseAccessor(dbURL, dbName, dbColl)
 	if err != nil {
 		panic(err)
@@ -110,6 +177,11 @@ func main() {
 	router.HandleFunc("/binding", bindingHandler).Methods("POST")
 	router.HandleFunc("/sign-in", signInHandler).Methods("POST")
 	router.HandleFunc("/users", usersHandler).Methods("GET")
+	router.HandleFunc("/login", jwtLoginHandler).Methods("POST")
+	router.Handle("/secured", negroni.New(
+		negroni.HandlerFunc(jwtMiddleware.HandlerWithNext),
+		negroni.Wrap(http.HandlerFunc(jwtSecuredHandler)),
+	))
 
 	n.Use(nigronimgosession.NewDatabase(*dbAccessor).Middleware())
 	n.UseHandler(router)
