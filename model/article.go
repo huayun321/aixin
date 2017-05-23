d7eb0421f86e0b8155041300b013711696640fde
huayun321 <huayun321@gmail.com>
2017-05-23 19:03:29 +0800

    add article model

--- model/article.go
+++ model/article.go
@@ -8,8 +8,6 @@ type Article struct {
 	Author        User          `json:"author" bson:"author,omitempty"`
 	Content       string        `json:"content" bson:"content,omitempty"`
 	Images        []string      `json:"images,omitempty" bson:"images,omitempty"`
-	Comments      []Comment     `json:"comments,omitempty" bson:"comments,omitempty"`
-	Fans          []User        `json:"fans,omitempty" bson:"fans,omitempty"`
 	IsSelected    bool          `json:"is_selected" bson:"is_selected,omitempty"`
 	IsDeleted     bool          `json:"is_deleted" bson:"is_deleted,omitempty"`
 	SelectTime    int64         `json:"select_time,omitempty" bson:"select_time,omitempty"`
@@ -20,9 +18,21 @@ type Article struct {
 
 //Comment 用户回复
 type Comment struct {
-	ID            bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	Author        User          `json:"author" bson:"author,omitempty"`
-	Content       string        `json:"content" bson:"content,omitempty"`
-	Reference     Comment       `json:"images,omitempty" bson:"images,omitempty"`
-	CreateTime    int64         `json:"create_time,omitempty" bson:"create_time,omitempty"`
+	ID          bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Author      User          `json:"author" bson:"author,omitempty"`
+	Content     string        `json:"content" bson:"content,omitempty"`
+	ReferenceID bson.ObjectId `json:"reference_id,omitempty" bson:"reference_id,omitempty"`
+	CreateTime  int64         `json:"create_time,omitempty" bson:"create_time,omitempty"`
+}
+
+type Fan struct {
+	ID        bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	ArticleID bson.ObjectId `json:"article_id" bson:"article_id,omitempty"`
+	UserID    bson.ObjectId `json:"user_id" bson:"user_id,omitempty"`
+}
+
+type Bookmark struct {
+	ID        bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	ArticleID bson.ObjectId `json:"article_id" bson:"article_id,omitempty"`
+	UserID    bson.ObjectId `json:"user_id" bson:"user_id,omitempty"`
 }
