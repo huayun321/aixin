50c5ac54d27271d733c9a2caef3aef6e822f3c76
huayun321 <huayun321@gmail.com>
2017-06-12 22:53:49 +0800

    fix news model

--- model/news.go
+++ model/news.go
@@ -2,16 +2,17 @@ package model
 
 import "gopkg.in/mgo.v2/bson"
 
-//Article 用户帖子
+//News 资讯
 type News struct {
 	ID            bson.ObjectId `json:"id" bson:"_id,omitempty"`
 	AuthorId      bson.ObjectId `json:"author_id" bson:"author_id,omitempty"`
 	Content       string        `json:"content" bson:"content,omitempty"`
-	Image        string      `json:"image,omitempty" bson:"image,omitempty"`
-	IsDeleted     bool          `json:"is_deleted" bson:"is_deleted,omitempty"`
-	DeleteTime    int64         `json:"delete_time,omitempty" bson:"delete_time,omitempty"`
+	Image         string        `json:"image,omitempty" bson:"image,omitempty"`
+	IsPublished   bool          `json:"is_published" bson:"is_published,omitempty"`
+	PublishTime   int64         `json:"publish_time,omitempty" bson:"publish_time,omitempty"`
 	CreateTime    int64         `json:"create_time,omitempty" bson:"create_time,omitempty"`
 	BookmarkCount int           `json:"bookmark_count" bson:",omitempty"`
 	Author        User          `json:"author" bson:",omitempty"`
 	Comments      []Comment     `json:"comments"  bson:",omitempty"`
+	Position      int           `json:"position"`
 }
