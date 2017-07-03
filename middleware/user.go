779bd616e532d69813593843126c1f1d3313744a
huayun321 <huayun321@gmail.com>
2017-07-03 18:35:14 +0800

    fix isuserm

--- middleware/user.go
+++ middleware/user.go
@@ -25,10 +25,10 @@ func IsAdminM(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
 
 //IsUserM 验证是否为user
 func IsUserM(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
-	fmt.Println("IsAdminM")
+	fmt.Println("is user m")
 	user := r.Context().Value("user")
 
-	if user.(*jwt.Token).Claims.(jwt.MapClaims)["role"] != "admin" {
+	if user.(*jwt.Token).Claims.(jwt.MapClaims)["role"] != "user" {
 		util.Ren.JSON(w, http.StatusUnauthorized, map[string]interface{}{
 			"code":    10001,
 			"message": "该用户没有此权限",
